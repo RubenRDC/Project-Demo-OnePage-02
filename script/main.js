26c44467024f506a6294b502bfd29502d2078a2e
Ruben <rubenrdc2003@gmail.com>
2025-03-16 12:46:10 -0300

    Proyecto ya iniciado.

---
+++ script/main.js
@@ -0,0 +1,17 @@
+const btnMenu = document.getElementById("btn_menu");
+const menu = document.getElementById("menu");
+btnMenu.addEventListener("click", (e) => {
+    switchStatusMenu()
+})
+menu.addEventListener("click", (e) => {
+    switchStatusMenu()
+})
+function switchStatusMenu() {
+    if (menu.classList.contains("show")) {
+        menu.classList.remove("show");
+        btnMenu.classList.remove("active");
+    } else {
+        menu.classList.add("show");
+        btnMenu.classList.add("active");
+    }
+}
